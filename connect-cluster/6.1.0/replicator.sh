ecb77218bf92836d7ece65467c212d6cf990df76
Alex Bleasdale <ableasdale@confluent.io>
2022-03-02 22:14:07 +0000

    commit

---
+++ connect-cluster/6.1.0/replicator.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+HEADER="Content-Type: application/json"
+DATA=$( cat << EOF
+{
+  "name": "replicator-dc1-to-dc2-topic1",
+  "config": {
+    "connector.class": "io.confluent.connect.replicator.ReplicatorSourceConnector",
+    "topic.whitelist": "replicate-me",
+    "key.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+    "value.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+    "src.kafka.bootstrap.servers": "broker:9092",
+    "src.consumer.group.id": "replicator-dc1-to-dc2-topic1",
+    "src.kafka.timestamps.topic.replication.factor": 1,
+    "dest.kafka.bootstrap.servers": "broker:9092",
+    "confluent.topic.replication.factor": 1,
+    "header.converter": "io.confluent.connect.replicator.util.ByteArrayConverter",
+    "tasks.max": "1",
+    "topic.rename.format":"backup_${topic}"
+  }
+}
+EOF
+)
+
+curl -X POST -H "${HEADER}" --data "${DATA}" http://localhost:8083/connectors
