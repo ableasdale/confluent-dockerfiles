227526f65fb89d4e7a4459075149f40c7f187968
Alex Bleasdale <ableasdale@confluent.io>
2023-03-22 21:27:55 +0000

    broker1 appears to be starting up with the TLS/SSL listener

--- cluster-linking-mTLS/security1/create-certs.sh
+++ cluster-linking-mTLS/security1/create-certs.sh
@@ -1,14 +1,18 @@
-echo 1
+echo "Step 1"
 openssl req -new -newkey rsa:4096 -days 365 -x509 -subj "/CN=Kafka-Security-CA" -keyout ca-key -out ca-cert -nodes
-echo 2
+echo "Step 2"
 keytool -genkey -keystore kafka.server.keystore.jks -validity 365 -storepass confluent -keypass confluent -dname "CN=broker1" -storetype pkcs12 -keyalg RSA
-echo 3
+echo "Step 3"
 keytool -keystore kafka.server.keystore.jks -certreq -file cert-file -storepass confluent -keypass confluent
-echo 4
+echo "Step 4"
 openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file -out cert-signed -days 365 -CAcreateserial -passin pass:confluent
-echo 5
+echo "Step 5"
 keytool -keystore kafka.server.truststore.jks -alias CARoot -import -file ca-cert -storepass confluent -keypass confluent -noprompt -keyalg RSA
-echo 6
+echo "Step 6"
 keytool -keystore kafka.server.keystore.jks -alias CARoot -import -file ca-cert -storepass confluent -keypass confluent -noprompt
-echo 7
+echo "Step 7"
 keytool -keystore kafka.server.keystore.jks -import -file cert-signed -storepass confluent -keypass confluent -noprompt
+echo "Step 8"
+tee broker_sslkey_creds << EOF >/dev/null
+confluent
+EOF
