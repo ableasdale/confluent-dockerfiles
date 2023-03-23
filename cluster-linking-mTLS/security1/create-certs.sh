14def156939184a731ee0453c91f047789e8450f
Alex Bleasdale <ableasdale@confluent.io>
2023-03-23 14:52:21 +0000

    Adding latest version of the certificate script

--- cluster-linking-mTLS/security1/create-certs.sh
+++ cluster-linking-mTLS/security1/create-certs.sh
@@ -1,19 +1,19 @@
 echo "Step 1: Root Certificate (CA)"
 openssl req -new -newkey rsa:4096 -days 365 -x509 -subj "/CN=Kafka-Security-CA" -keyout ca-key -out ca-cert -nodes
 
-echo "Step 2"
+echo "Step 2: Server Keystore"
 keytool -genkey -keystore kafka.server.keystore.jks -validity 365 -storepass confluent -keypass confluent -dname "CN=broker1" -storetype pkcs12 -keyalg RSA
-echo "Step 3"
+echo "Step 3: Adding Certificate to Keystore"
 keytool -keystore kafka.server.keystore.jks -certreq -file cert-file -storepass confluent -keypass confluent
-echo "Step 4"
+echo "Step 4: Sign Server Certificate"
 openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file -out cert-signed -days 365 -CAcreateserial -passin pass:confluent
-echo "Step 5"
+echo "Step 5: Server Truststore"
 keytool -keystore kafka.server.truststore.jks -alias CARoot -import -file ca-cert -storepass confluent -keypass confluent -noprompt -keyalg RSA
-echo "Step 6"
+echo "Step 6: Import the signed client certificate and the CA into the keystore"
 keytool -keystore kafka.server.keystore.jks -alias CARoot -import -file ca-cert -storepass confluent -keypass confluent -noprompt
-echo "Step 7"
+echo "Step 7: Add Certificate Reply"
 keytool -keystore kafka.server.keystore.jks -import -file cert-signed -storepass confluent -keypass confluent -noprompt
-echo "Step 8"
+echo "Step 8: Create credentials file"
 tee broker_sslkey_creds << EOF >/dev/null
 confluent
 EOF
@@ -29,3 +29,5 @@ echo "Step 13: Import the signed client certificate and the CA into the keystore
 keytool -keystore kafka.client.keystore.jks -alias CARoot -import -file ca-cert -storepass confluent -keypass confluent -noprompt
 echo "Step 13: Add Certificate Reply"
 keytool -keystore kafka.client.keystore.jks -import -file client-cert-signed -alias my-local-pc -storepass confluent -keypass confluent -noprompt
+echo "Step 14: Create PEM File"
+cat ca.crt ca.key > ca.pem
